e95fe963b27c1c397721cb27dff7f9f867b20411
vsoch <vsoch@users.noreply.github.com>
2021-10-18 11:35:28 -0600

    add example smeagle output
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ cli/load.go
@@ -0,0 +1,34 @@
+package cli
+
+// This is just for debugging - it loads and prints the json corpus
+
+import (
+	"fmt"
+	"github.com/DataDrake/cli-ng/v2/cmd"
+	"github.com/vsoch/gosmeagle/corpus"
+)
+
+type LoadArgs struct {
+	JsonFile []string `desc:"A binary to parse."`
+}
+type LoadFlags struct{}
+
+// Parser looks at symbols and ABI in Go
+var Loader = cmd.Sub{
+	Name:  "load",
+	Alias: "l",
+	Short: "Test laoding a Json output",
+	Flags: &LoadFlags{},
+	Args:  &LoadArgs{},
+	Run:   RunLoader,
+}
+
+func init() {
+	cmd.Register(&Loader)
+}
+
+func RunLoader(r *cmd.Root, c *cmd.Sub) {
+	args := c.Args.(*LoadArgs)
+	C := corpus.Load(args.JsonFile[0])
+	fmt.Println(C)
+}
